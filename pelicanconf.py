59cfa982d8a9ef6156cdd116a1a83f99a612ec5f
Fabrizio (Misto) Milo <mistobaan@gmail.com>
2014-04-24 21:12:44 -0700

    add autogenerated scripts

---
+++ pelicanconf.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Fabrizio Milo'
+SITENAME = u'Fabrizio Milo'
+SITEURL = ''
+
+TIMEZONE = 'US/Pacific'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS =  (('Pelican', 'http://getpelican.com/'),
+          ('Python.org', 'http://python.org/'),
+          ('Jinja2', 'http://jinja.pocoo.org/'),
+        )
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+# Following items are often useful when publishing
+
+SOCIAL = (('twitter', 'http://twitter.com/fabmilo'),
+          ('linkedin', 'http://www.linkedin.com/in/fmilo'),
+          ('github', 'http://github.com/Mistobaan'),
+          ('stackoverflow', 'http://stackoverflow.com/users/121687/fabriziom')
+)
+
+
+DISQUS_SITENAME = "fabriziomilocom"
+GOOGLE_ANALYTICS = "UA-50383115-1"
+
+CC_LICENSE="CC-BY"
+
+GITHUB_USER="Mistobaan"
+
+THEME="theme"
